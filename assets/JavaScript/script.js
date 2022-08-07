a4a3d1bcb250f35b5825a44e3cd4a600b588f0e7
ejackson1228 <ejackson1228@gmail.com>
2022-08-07 19:25:57 -0400

    added repeat code for question 2

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -18,6 +18,7 @@ var createQuestion1 = function() {
     firstpage.remove();
     //elements for question 1
     var page2 = document.createElement("div");
+    page2.id = "page2"
     document.querySelector("#pagecontainer");
     pagecontainer.appendChild(page2);
     
@@ -60,17 +61,17 @@ var createQuestion1 = function() {
     q1AnswerList.appendChild(innerList1Div2);
     innerList1Div2.appendChild(q1a2);
     
-    var innerList1Div3 = document.createElement("div");
-    innerList1Div3.className = "correct";
-    q1AnswerList.appendChild(innerList1Div3);
-    innerList1Div3.appendChild(q1a3);
+    var innerList2Div3 = document.createElement("div");
+    innerList2Div3.className = "correct";
+    q1AnswerList.appendChild(innerList2Div3);
+    innerList2Div3.appendChild(q1a3);
     
-    var innerList1Div4 = document.createElement("div");
-    innerList1Div4.className = "incorrect";
-    q1AnswerList.appendChild(innerList1Div4);
-    innerList1Div4.appendChild(q1a4);
+    var innerList2Div4 = document.createElement("div");
+    innerList2Div4.className = "incorrect";
+    q1AnswerList.appendChild(innerList2Div4);
+    innerList2Div4.appendChild(q1a4);
     
-    // loop through all possible incorrect buttons to return alert that it's wrong
+    // loop through all possible incorrect buttons to return alert that it's wrong and subtract time 
 var incorrectAnswers = document.getElementsByClassName("incorrect"); 
 for (var i=0; i < incorrectAnswers.length; i++) {
     incorrectAnswers[i].addEventListener("click", function() {
@@ -83,8 +84,86 @@ var correctAnswers = document.getElementsByClassName("correct");
 for (var i=0; i < correctAnswers.length; i++) {
     correctAnswers[i].addEventListener("click", function() {
         correctAlert();
+        createQuestion2();
     })
 };
+}
+
+var createQuestion2 = function (){
+    // removing elements from question 1
+    document.getElementById("#page2");
+    page2.remove();
+    //creating elements for question 2
+    var page3 = document.createElement("div");
+    page3.id = "page3"
+    document.querySelector("#pagecontainer");
+    pagecontainer.appendChild(page3);
+    
+    var question2 = document.createElement("h2");
+    question2.className = "questions"
+    question2.innerHTML = "<h2 class='questions'>What does CSS stand for?</h2>";
+    page3.appendChild(question2)
+    
+    var q2AnswerList = document.createElement("ul");
+    q2AnswerList.className = "answerslist";
+    page3.appendChild(q2AnswerList);
+    
+    var q2a1 = document.createElement("li");
+    q2a1.className = "answers"
+    q2a1.id = "incorrect1";
+    q2a1.innerHTML = "<button class='answersbutton'>Capture Santa's Sleigh</button>";
+    
+    var q2a2 = document.createElement("li");
+    q2a2.className= "answers"
+    q2a2.id = "incorrect2";
+    q2a2.innerHTML  = "<button class='answersbutton'>Clear Selected Styles</button>";
+    
+    var q2a3 = document.createElement("li");
+    q2a3.className = "answers";
+    q2a3.id = "incorrect3";
+    q2a3.innerHTML = "<button class='answersbutton'>Cascading Styling Selector</button>";
+    
+    var q2a4 = document.createElement("li");
+    q2a4.className = "answers";
+    q2a4.id = "correct";
+    q2a4.innerHTML = "<button class='answersbutton'>Cascading StyleSheet</button>";
+    
+    var innerList2Div1 = document.createElement("div");
+    innerList2Div1.className = "incorrect";
+    q2AnswerList.appendChild(innerList2Div1);
+    innerList2Div1.appendChild(q2a1);
+    
+    var innerList2Div2 = document.createElement("div");
+    innerList2Div2.className = "incorrect";
+    q2AnswerList.appendChild(innerList2Div2);
+    innerList2Div2.appendChild(q2a2);
+    
+    var innerList2Div3 = document.createElement("div");
+    innerList2Div3.className = "incorrect";
+    q2AnswerList.appendChild(innerList2Div3);
+    innerList2Div3.appendChild(q2a3);
+    
+    var innerList2Div4 = document.createElement("div");
+    innerList2Div4.className = "correct";
+    q2AnswerList.appendChild(innerList2Div4);
+    innerList2Div4.appendChild(q2a4);
+
+    // loop through all possible incorrect buttons to return alert that it's wrong and subtract time 
+    var incorrectAnswers = document.getElementsByClassName("incorrect"); 
+    for (var i=0; i < incorrectAnswers.length; i++) {
+        incorrectAnswers[i].addEventListener("click", function() {
+            incorrectAlert();
+            timeLeft = timeLeft - 10;
+        })
+    };
+// loop through all possible correct answers to return alert if it's right
+    var correctAnswers = document.getElementsByClassName("correct"); 
+    for (var i=0; i < correctAnswers.length; i++) {
+        correctAnswers[i].addEventListener("click", function() {
+            correctAlert();
+            // createQuestion3();
+        })
+    };
 }
 
 
